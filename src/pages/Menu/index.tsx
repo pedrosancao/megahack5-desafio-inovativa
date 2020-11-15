9b39f3702b277ac27c3090b509bb788d5c8b24d9
Pedro Sanção <pedrosancao@users.noreply.github.com>
2020-11-14 22:42:15 -0300

    Cria botões do menu

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 
-import { Container } from './styles';
+import { Container, Nav } from './styles';
 
 const Menu: React.FC = () => {
   return (
     <Container>
-      <NavLink to="jornada">minha jornada</NavLink>
-      <NavLink to="mentoria">mentoria</NavLink>
-      <NavLink to="admin">admin</NavLink>
+      <Nav to="jornada">minha jornada</Nav>
+      <Nav to="mentoria">mentoria</Nav>
+      <Nav to="admin">admin</Nav>
     </Container>
   );
 };
--- src/pages/Menu/styles.ts
+++ src/pages/Menu/styles.ts
@@ -1,3 +1,25 @@
 import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 500px;
+  margin: 1rem auto 3rem;
+  padding: 0 1rem;
+`;
+export const Nav = styled(NavLink)`
+  padding: 0.5rem 1rem;
+  border-radius: 0.5rem;
+  border: 1px #666 solid;
+  text-decoration: none;
+  text-align: center;
+  color: #333;
+  font-size: 2rem;
+  &:hover {
+    background-color: #ccc;
+  }
+  & + & {
+    margin-top: 1rem;
+  }
+`;
