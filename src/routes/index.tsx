280efb2019964f45ab96b8800ed998e0b23a67d8
Pedro Sanção <pedrosancao@users.noreply.github.com>
2020-11-14 22:42:47 -0300

    Redireciona para página da jornada

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 import Admin from '../pages/Admin';
 import Journey from '../pages/Journey';
 import Mentoria from '../pages/Mentoria';
-import Menu from '../pages/Menu';
+// import Menu from '../pages/Menu';
 
 const Routes: React.FC = () => (
   <Switch>
-    <Route path="/" exact component={Menu} />
+    <Route
+      path="/"
+      exact
+      // component={Menu}
+      render={() => <Redirect to="/jornada" />}
+    />
     <Route path="/jornada" component={Journey} />
     <Route path="/mentoria" component={Mentoria} />
     <Route path="/admin" component={Admin} />
